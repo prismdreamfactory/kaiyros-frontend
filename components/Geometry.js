ef23421d5001f15d3dae7f6c6eb9e642f7944ccb
Eric K Wu <ekuan.wu@gmail.com>
2019-09-11 16:53:31 -0700

    Remove vendor prefixes

--- components/Geometry.js
+++ components/Geometry.js
@@ -98,40 +98,12 @@ const GeometryStyles = styled.div`
   }
 
   .rotate {
-    -webkit-animation-name: spin;
-    -webkit-animation-duration: 60s;
-    -webkit-animation-iteration-count: infinite;
-    -webkit-animation-timing-function: linear;
-    -moz-animation-name: spin;
-    -moz-animation-duration: 60s;
-    -moz-animation-iteration-count: infinite;
-    -moz-animation-timing-function: linear;
-    -ms-animation-name: spin;
-    -ms-animation-duration: 60s;
-    -ms-animation-iteration-count: infinite;
-    -ms-animation-timing-function: linear;
-
     animation-name: spin;
     animation-duration: 60s;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
   }
-  @-moz-keyframes spin {
-    from {
-      -moz-transform: rotate(0deg);
-    }
-    to {
-      -moz-transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes spin {
-    from {
-      -webkit-transform: rotate(0deg);
-    }
-    to {
-      -webkit-transform: rotate(360deg);
-    }
-  }
+
   @keyframes spin {
     from {
       transform: rotate(0deg);
