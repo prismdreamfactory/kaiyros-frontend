425ab5e2a932a35339aa48c6373cf3ebd2a8fd11
Winston Wu <pfunto.wu@gmail.com>
2019-09-12 23:06:43 -0700

    switched config url to https

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // let wpUrl = 'http://localhost:8080/wp-json';
-let wpUrl = 'http://api.kaiyros.net/wp-json';
+let wpUrl = 'https://api.kaiyros.net/wp-json';
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
