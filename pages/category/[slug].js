068419cd25ccf3ea5a5b46eab1e0c099a151f9a7
Eric K Wu <ekuan.wu@gmail.com>
2019-10-24 18:00:29 -0700

    Category slug padding

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -89,7 +89,7 @@ const CategoryContainer = styled.div`
   }
 
   @media (max-width: 1024px) {
-    margin-top: 6rem;
+    margin-top: 8rem;
     .post-layout {
       grid-template-columns: 1fr;
     }
